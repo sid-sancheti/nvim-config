503e464d0e06df298e667a670577e48c93b5b19b
Siddharth Sancheti <sidsancheti94@gmail.com>
2024-03-21 22:16:31 -0400

    IBL: Disable the underlining and changed bar width

--- lua/sancheti/plugins/indent-blankline.lua
+++ lua/sancheti/plugins/indent-blankline.lua
@@ -1,5 +1,17 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
-  opts = {}
+  config = function()
+    local ibl = require("ibl")
+    ibl.setup({
+      enabled = true,
+      scope = {
+        show_start = false,
+        show_end = false,
+      },
+      indent = {
+        char = '│',
+      }
+    })
+  end,
 }
