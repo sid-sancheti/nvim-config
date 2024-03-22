af5bc8e033fcdf12ff81f6fa988a407bd3181e4d
Siddharth Sancheti <sidsancheti94@gmail.com>
2024-03-21 22:18:18 -0400

    Changed treesitter to look cleaner and added markdown file display

---
+++ lua/sancheti/plugins/headlines.lua
@@ -0,0 +1,9 @@
+--[[
+-- Configure the way markdown files appear in neovim.
+--]]
+
+return {
+  "lukas-reineke/headlines.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  opts = {}
+}
